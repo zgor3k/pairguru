b7db03aa7b59b1e04ee3b8a32a6bb96b140538ce
Marcin Zgórecki <marcin.zgorecki@kantar.com>
2020-01-27 21:45:30 +0100

    Add top commenters page
    TASK-6

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :top_commenters, only: [:index]
+
   resources :movies, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
 
